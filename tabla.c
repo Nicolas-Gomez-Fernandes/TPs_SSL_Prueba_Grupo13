5183530638be5d879d1b9c33ad205ceb77f58d43
Nicolas Gomez <ngomezfernandes@frba.utn.edu.ar>
2024-07-17 16:02:38 -0300

    subi la tabla.c

--- tabla.c
+++ tabla.c
@@ -1,1 +1,18 @@
 #include "tabla.h"
+#include "conversion.h"
+#include <stdio.h>
+#include <assert.h>
+
+int numeroPorDefecto = 1;
+void armarTablaDeConversion(float valorInicialNudos, float valorFinalNudos, float saltoDeValores){
+    assert(valorInicialNudos >= 0 && valorFinalNudos >= valorInicialNudos && saltoDeValores > 0);
+    if(numeroPorDefecto == 1){
+        printf("|   Nudos       |   Km/h      |\n");
+        printf("|---------------|-------------|\n");
+        numeroPorDefecto = 2; 
+    }
+    for (float valorActualNudos = valorInicialNudos; valorActualNudos <= valorFinalNudos; valorActualNudos += saltoDeValores) {
+        float kmh = convetirNudosAKilometrosPorHora(valorActualNudos); 
+        printf("|   %7.2f     |   %7.4f   |\n", valorActualNudos, kmh); 
+    }
+}

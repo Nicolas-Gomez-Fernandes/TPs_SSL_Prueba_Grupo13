06379a11d4e8f9a738f4dadd52ad688ef885df08
Nicolas Gomez <ngomezfernandes@frba.utn.edu.ar>
2024-07-17 16:03:06 -0300

    Update tabla.h

--- tabla.h
+++ tabla.h
@@ -1,1 +1,7 @@
-#include <stdio.h>
+#ifndef TABLA_H_INCLUDED
+#define TABLA_H_INCLUDED
+
+void armarTablaDeConversion(float valorInicialNudos, float valorFinalNudos, float saltoDeValores);
+extern int numeroPorDefecto;
+
+#endif
